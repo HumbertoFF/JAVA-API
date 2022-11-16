bd00a427e4237bc94b7492c923e64240efc85e1e
humberto <humberto.filho.091@ufrn.edu.br>
2022-11-16 00:03:43 -0300

    dados

---
+++ src/main/java/ufrn/br/ProjetoRestJustica/postgre.sql
@@ -0,0 +1,17 @@
+INSERT INTO pessoa_fisica ( name, email) VALUES ("name":"Humberto","email": "email@email.com");
+INSERT INTO pessoa_fisica ( name, email) VALUES ("name":"JOÃO DE TESTE","email": "email@email.com");
+INSERT INTO pessoa_fisica ( name, email) VALUES ("name":"PEDRO","email": "email@email.com");
+INSERT INTO pessoa_fisica ( name, email) VALUES ("name":"MATEUS","email": "email@email.com");
+
+INSERT INTO pessoa_juridica ( name, cnpj) VALUES ("name":"INSS", "cnpj": "235451358463123");
+INSERT INTO pessoa_juridica ( name, cnpj) VALUES ("name":"Caixa Econômica", "cnpj": "3654321531531");
+INSERT INTO pessoa_juridica ( name, cnpj) VALUES ("name":"União", "cnpj": "235451358463123");
+INSERT INTO pessoa_juridica ( name, cnpj) VALUES ("name":"Prefeitura", "cnpj": "3546432121355");
+
+INSERT INTO doenca (codigo, nome) VALUES ( "codigo": "13215", "nome":"cólera");
+INSERT INTO doenca (codigo, nome) VALUES ( "codigo": "12333", "nome":"varíula");
+INSERT INTO doenca (codigo, nome) VALUES ( "codigo": "12463", "nome":"Gota");
+INSERT INTO doenca (codigo, nome) VALUES ( "codigo": "12213", "nome":"Febre");
+INSERT INTO doenca (codigo, nome) VALUES ( "codigo": "12322", "nome":"Catapora");
+INSERT INTO doenca (codigo, nome) VALUES ( "codigo": "12366", "nome":"Câncer");
+
